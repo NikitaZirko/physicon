9165add0698841bc486580711552866145157275
Nikita Zirko <nik.message@gmail.com>
2020-05-14 16:50:13 +0200

    added basic settings

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Nav
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "navbar"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,15 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'Тестовое задание', content: 'Тестовое задание' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+            'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap'
+      }
     ]
   },
   /*
